ac66a9c345038a19d0903716d9e95a197521496a
d.bibikov <d.bibikov@crpt.ru>
2025-01-08 21:17:36 +0300

    update telescope-ui-select

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,18 @@
 return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+
   },
   {
     "nvim-telescope/telescope.nvim",
@@ -8,8 +20,6 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup()
-
-      require("telescope").load_extension("ui-select")
     end,
-  },
+  }
 }

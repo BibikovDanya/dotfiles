c3f5ebb65d21e307cc535b4955803673ef72f32b
d.bibikov <d.bibikov@crpt.ru>
2025-01-12 14:32:25 +0300

    update kemaps

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -28,6 +28,8 @@ end
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+-- Open filebar   
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 --------------- for plugins ---------------
 -- telescope
